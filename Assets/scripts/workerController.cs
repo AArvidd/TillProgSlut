712896e9f5e2e874630e135978b615a446fb907d
AArvidd <arvid.oscarsson@elev.ga.ntig.se>
2024-03-19 09:40:48 +0100

    added navigation and starged making the worker attack the tree

--- Assets/scripts/workerController.cs
+++ Assets/scripts/workerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -21,14 +23,32 @@ public class workerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(agent.remainingDistance);
+        if(target == null)
+            setTarget();
+        agent.destination = target.transform.position;
+        attack();
     }
+
+    private void attack(){
+        //todo make atach tree
+
+    }
+    
 
     private void setTarget(){
         GameObject[] trees = GameObject.FindGameObjectsWithTag("tree");
+        float shortest = 9999999999999999999;
+        int shortestI = -1;
         for (int i = 0; i < trees.Length; i++){
-            //TODO afindes the closest tree
+            Vector3 curent = trees[i].transform.position;
+            agent.destination = curent;
+            float testLenth = agent.remainingDistance;
+            if(testLenth < shortest){
+                shortest = testLenth;
+                shortestI = i;
+            }
         }
+        target = trees[shortestI];
     }
 
 }
